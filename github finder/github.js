fdaad6a0a73ef0dd17fb56f2e08a3c5a7cec6135
Andre Ostan <andre.ostan@gmail.com>
2022-08-30 21:07:04 -0300

    Part 4 authorization fix

--- github finder/github.js
+++ github finder/github.js
@@ -1,12 +1,16 @@
 class GitHub {
   constructor() {
-    this.client_id = "620e04a630a318920a25";
-    this.client_secret = "5cad3aee5a54a964b5c6f84608cbbfc6013a9548";
+    this.config = {
+      headers: {
+        Authorization: "token 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",
+      },
+    };
   }
 
   async getUser(user) {
     const profileResponse = await fetch(
-      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}`,
+      this.config
     );
 
     const profile = await profileResponse.json();
